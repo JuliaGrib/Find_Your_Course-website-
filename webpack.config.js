643b8e3740e918872830f2e6aeed0f7b44dabac5
Julia Grib <yulgribgmail.com>
2023-02-03 14:34:52 +0300

    start project

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.pug',
+      filename: 'index.html',
+      scriptLoading: "blocking",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.pug$/,
+        loader: '@webdiscus/pug-loader',
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      }
+    ],
+  },
+  devServer: {
+    watchFiles: path.join(__dirname, 'src'),
+    port: 9000,
+    hot: true,
+  }
+};
